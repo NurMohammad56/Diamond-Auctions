d885f5024bbb4552eef2e0317274a207a009cb51
NurMohammad56 <milonsardar56@gmail.com>
2025-04-15 15:37:12 +0600

    feat(auction): implement pagination for get all auctions; refactor response structure and error handling

--- src/routes/auction.routes.js
+++ src/routes/auction.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createAuctionData, uploadAuctionImages } from '../controllers/auction.controller.js'
+import { createAuctionData, uploadAuctionImages, getAllAuctions } from '../controllers/auction.controller.js'
 import upload from "../middlewares/multer.middlewares.js";
 import {verifyJWT} from "../middlewares/auth.middlewares.js"
 import { isSeller } from '../middlewares/role.middlewares.js'
@@ -10,6 +10,7 @@ const router = express.Router()
 
 router.post("/create-auction", verifyJWT, isSeller, createAuctionData);
 router.post("/upload-auction-images/:id", verifyJWT, isSeller, upload.array('images', 10), uploadAuctionImages);
+router.get("/get-all-auctions", getAllAuctions);
 
 
 export default router
