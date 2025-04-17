03a87ecd3f8197257b97acde38d526eef25b4cd2
NurMohammad56 <milonsardar56@gmail.com>
2025-04-17 11:05:06 +0600

    feat(bid): add getBid endpoint to retrieve a specific bid; enhance response structure for bid retrieval

--- src/routes/bid.routes.js
+++ src/routes/bid.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import {
   placeBid,
-  getUserBids
-
+  getUserBids,
+  getBid
 } from '../controllers/bid.controllers.js';
 import { verifyJWT } from '../middlewares/auth.middlewares.js';
 
@@ -10,5 +10,6 @@ const router = express.Router();
 
 router.post('/auctions/:auctionId/bid',verifyJWT, placeBid);
 router.get('/users/:userId/bid', verifyJWT, getUserBids);   
+router.get('/:id/bid', verifyJWT, getBid);
 
 export default router;
