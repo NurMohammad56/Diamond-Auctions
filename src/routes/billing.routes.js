f09aba03acbfa23a9f5b0b1303411ce5a39713fd
NurMohammad56 <milonsardar56@gmail.com>
2025-04-29 09:50:57 +0600

    feat(billing): implement user authentication for billing operations and enhance billing model

--- src/routes/billing.routes.js
+++ src/routes/billing.routes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
-import {getBillings, createBilling} from '../controllers/billing.controllers.js';
-router.post('/create', createBilling);
-router.get('/get', getBillings);
+import {getBillings, createBilling, getBillingById, updateBilling, deleteBilling} from '../controllers/billing.controllers.js';
+import { verifyJWT } from '../middlewares/auth.middlewares.js';
+router.post('/create', verifyJWT, createBilling);
+router.get('/get',verifyJWT, getBillings);
+router.get('/get/:id',verifyJWT, getBillingById);
+router.put('/update/:id',verifyJWT, updateBilling);
+router.delete('/delete/:id',verifyJWT, deleteBilling);
 
 export default router;
